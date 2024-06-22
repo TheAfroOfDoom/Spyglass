500dd766fb95c33817091b59e9438fa6ded3b3ec
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-06-22 18:02:08 -0400

    ♻️ Build `core.string` parser result fields independently - ...instead of collecting them on the result object directly as we go - this is so we can re-assign `valueMap` with `valueMap = IndexMap.merge(...)` later - tests should still pass, this should be 0 diff

--- packages/core/src/parser/string.ts
+++ packages/core/src/parser/string.ts
@@ -4,26 +4,23 @@ import type { Quote, StringNode, StringOptions } from '../node/index.js'
 import { EscapeChar, EscapeTable } from '../node/index.js'
 import type { InfallibleParser } from '../parser/index.js'
 import type { ParserContext } from '../service/index.js'
-import type { IndexMap } from '../source/index.js'
-import { Range, Source } from '../source/index.js'
+import { IndexMap, Range, Source } from '../source/index.js'
 import type { Parser, Result, Returnable } from './Parser.js'
 import { Failure } from './Parser.js'
 
 export function string(options: StringOptions): InfallibleParser<StringNode> {
 	return (src: Source, ctx: ParserContext): StringNode => {
-		const ans: StringNode = {
-			type: 'string',
-			range: Range.create(src),
-			options,
-			value: '',
-			valueMap: [],
-		}
-		let start = src.cursor
+		let value = ''
+		let children
+		let quote
+		let start = src.innerCursor
+		const range = Range.create(src)
+		const valueMap = []
 
 		if (options.quotes?.length && (src.peek() === '"' || src.peek() === "'")) {
 			const currentQuote = src.read() as Quote
-			ans.quote = currentQuote
-			const contentStart = src.cursor
+			quote = currentQuote
+			const contentStart = src.innerCursor
 			while (src.canRead() && src.peek() !== currentQuote) {
 				const c = src.peek()
 				if (options.escapable && c === '\\') {
@@ -35,30 +32,30 @@ export function string(options: StringOptions): InfallibleParser<StringNode> {
 						|| c2 === currentQuote
 						|| EscapeChar.is(options.escapable.characters, c2)
 					) {
-						ans.valueMap.push({
-							inner: Range.create(ans.value.length, ans.value.length + 1),
+						valueMap.push({
+							inner: Range.create(value.length, value.length + 1),
 							outer: Range.create(cStart, src),
 						})
-						ans.value += EscapeTable.get(c2)
+						value += EscapeTable.get(c2)
 					} else if (options.escapable.unicode && c2 === 'u') {
 						const hex = src.peek(4)
 						if (/^[0-9a-f]{4}$/i.test(hex)) {
 							src.skip(4)
-							ans.valueMap.push({
-								inner: Range.create(ans.value.length, ans.value.length + 1),
+							valueMap.push({
+								inner: Range.create(value.length, value.length + 1),
 								outer: Range.create(cStart, src),
 							})
-							ans.value += String.fromCharCode(parseInt(hex, 16))
+							value += String.fromCharCode(parseInt(hex, 16))
 						} else {
 							ctx.err.report(
 								localize('parser.string.illegal-unicode-escape'),
 								Range.create(src, src.getCharRange(3).end),
 							)
-							ans.valueMap.push({
-								inner: Range.create(ans.value.length, ans.value.length + 1),
+							valueMap.push({
+								inner: Range.create(value.length, value.length + 1),
 								outer: Range.create(cStart, src),
 							})
-							ans.value += c2
+							value += c2
 						}
 					} else {
 						if (!options.escapable.allowUnknown) {
@@ -67,15 +64,15 @@ export function string(options: StringOptions): InfallibleParser<StringNode> {
 								src.getCharRange(-1),
 							)
 						}
-						ans.valueMap.push({
-							inner: Range.create(ans.value.length, ans.value.length + 1),
+						valueMap.push({
+							inner: Range.create(value.length, value.length + 1),
 							outer: Range.create(cStart, src),
 						})
-						ans.value += c2
+						value += c2
 					}
 				} else {
 					src.skip()
-					ans.value += c
+					value += c
 				}
 			}
 
@@ -84,28 +81,38 @@ export function string(options: StringOptions): InfallibleParser<StringNode> {
 			}
 
 			if (!options.quotes.includes(currentQuote)) {
-				ctx.err.report(localize('parser.string.illegal-quote', options.quotes), ans)
+				ctx.err.report(localize('parser.string.illegal-quote', options.quotes), range)
 			}
 
 			start = contentStart
 		} else if (options.unquotable) {
 			while (src.canRead() && isAllowedCharacter(src.peek(), options.unquotable)) {
-				ans.value += src.read()
+				value += src.read()
 			}
-			if (!ans.value && !options.unquotable.allowEmpty) {
+			if (!value && !options.unquotable.allowEmpty) {
 				ctx.err.report(localize('expected', localize('string')), src)
 			}
 		} else {
 			ctx.err.report(localize('expected', options.quotes!), src)
 		}
 
-		ans.valueMap.unshift({ inner: Range.create(0), outer: Range.create(start) })
+		valueMap.unshift({ inner: Range.create(0), outer: Range.create(start) })
+
+		const ans: StringNode = {
+			type: 'string',
+			range: Range.create(src),
+			options,
+			value,
+			valueMap: IndexMap.merge(src.indexMap, valueMap),
+			quote,
+			children,
+		}
 
 		if (options.value?.parser) {
 			const valueResult = parseStringValue(options.value.parser, ans.value, ans.valueMap, ctx)
 			/* istanbul ignore else */
 			if (valueResult !== Failure) {
-				ans.children = [valueResult]
+				children = [valueResult]
 			}
 		}
 
